e386e0bf66cb50dfee8a026828537e7cdb2ec378
NoorAzam-1 <azamnoor698@gmail.com>
2026-07-11 17:35:34 +0530

    Refactor index.html to improve structure and clarity, adding section headers and correcting example descriptions

--- intro.js
+++ intro.js
@@ -1,2 +1,21 @@
 // console.log("hello ")
 console.log("hello ");
+
+
+const day = "tuesday1";
+
+switch (day) {
+    case "monday":
+        console.log("monday")
+        break;
+    case "tuesday":
+        console.log("tuesday")
+        break;
+    case "thursday":
+        console.log("thursday")
+        break;
+
+    default:
+        console.log("none of above")
+        break;
+}
